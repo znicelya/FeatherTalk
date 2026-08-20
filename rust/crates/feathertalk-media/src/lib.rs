cad792d7b0776480bbbf548171ba14a3e413bc8d
Nicely <2262009081@qq.com>
2026-08-20 17:05:08 +0800

    feat: validate media normalization layout

--- rust/crates/feathertalk-media/src/lib.rs
+++ rust/crates/feathertalk-media/src/lib.rs
@@ -1,5 +1,7 @@
 mod error;
 mod model;
+mod validate;
 
 pub use error::MediaError;
 pub use model::{MediaInput, NormalizationSpec, NormalizedMediaLayout, ValidatedInput};
+pub use validate::{validate_input, validate_normalization};
