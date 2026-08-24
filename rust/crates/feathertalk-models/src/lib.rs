dba7629106916ad4fc0654c49d7ec5bc1cf48ee8
Nicely <2262009081@qq.com>
2026-08-24 16:44:31 +0800

    feat: reparameterize shared MobileOne blocks

--- rust/crates/feathertalk-models/src/lib.rs
+++ rust/crates/feathertalk-models/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod backend;
 pub mod feather_hubert;
+mod mobileone;
 pub mod pfld;
 pub mod train_step;
 pub mod unet;
 
+pub use mobileone::{MobileOneBlock, ReparameterizedMobileOneBlock};
 pub use pfld::{
-    GhostOneBottleneck, GhostOneModule, MobileOneBlock, PFLD_GhostOne, PFLD_INPUT_CHANNELS,
-    PFLD_OUTPUT_VALUES, PfldConfig, PfldGhostOne,
+    GhostOneBottleneck, GhostOneModule, PFLD_GhostOne, PFLD_INPUT_CHANNELS, PFLD_OUTPUT_VALUES,
+    PfldConfig, PfldGhostOne,
 };
