3175c66089bb1a14cb2925661f92c60dce560dab
Nicely <2262009081@qq.com>
2026-09-04 11:00:17 +0800

    feat(models): add a trainable talking-head boundary

--- rust/crates/feathertalk-models/src/unet/mod.rs
+++ rust/crates/feathertalk-models/src/unet/mod.rs
@@ -7,6 +7,7 @@ mod inference;
 mod mobileone_blocks;
 mod mobileone_model;
 mod model;
+mod training_graph;
 
 pub use audio::AudioConvHubert;
 pub use blocks::{Down, InvertedResidual};
@@ -18,3 +19,4 @@ pub use inference::TalkingHeadModel;
 pub use mobileone_blocks::{MobileOneAudioConvHubert, MobileOneDown, MobileOneUp};
 pub use mobileone_model::{MobileOneUnet, MobileOneUnetInference};
 pub use model::OriginalUnet;
+pub use training_graph::TrainableTalkingHead;
